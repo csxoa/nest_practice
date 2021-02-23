288d9f5624d0db348855805c791fcf8ec619b693
sue517 <csooa9305@gmail.com>
2021-02-23 19:56:29 +0900

    add DTO & Validation

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { updateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -9,25 +11,24 @@ export class MoviesService {
         return this.movies;
     }
 
-    getOne(id:string): Movie {
-        return this.movies.find(movie => movie.id === parseInt(id))
-        // parseInt(id) = +id 라고 바꾸어도 같다.
+    getOne(id:number): Movie {
+        return this.movies.find(movie => movie.id === id)
     }
 
-    deleteOne(id:string):boolean {
+    deleteOne(id:number) {
         this.getOne(id)
-        this.movies = this.movies.filter(movie => movie.id !== +id)
+        this.movies = this.movies.filter(movie => movie.id !== id)
         return true;
     }
 
-    create(movieData){
+    create(movieData: CreateMovieDto){
         this.movies.push({
             id: this.movies.length + 1,
             ...movieData 
         })
     }
 
-    update(id:string, updateData) {
+    update(id:number, updateData: updateMovieDto) {
         const movie = this.getOne(id);
         this.deleteOne(id);
         this.movies.push({ ...movie, ...updateData })
